de9de8572638c087c227d5110daa2191d186d4f7
Joshua Miguel Bernal <joshua_bernal@outlook.com>
2025-01-23 19:32:24 +0800

    added start over functionality

--- src/Components/IngredientsList.jsx
+++ src/Components/IngredientsList.jsx
@@ -9,7 +9,7 @@ function IngredientsList(props) {
        <section className="my-6">
             <h2 className="font-bold text-2xl">Ingredients on hand:</h2>
             <ul className="list-disc text-gray-600 mx-10 my-4">{ingredientsListItems}</ul>
-            {props.ingArray.length >= 4 && <div className="flex my-8 py-4 px-8 bg-recipeContainer rounded-lg justify-between items-center">
+            {props.ingArray.length >= 4 && <div className="flex flex-col gap-y-3 my-8 py-4 px-8 bg-recipeContainer rounded-lg justify-between items-start sm:flex-row sm:items-center">
                 <div>
                     <h3 className="font-semibold py-2 text-md">Ready for a recipe?</h3>
                     <p className="py-2 text-sm text-gray-500">Generate a recipe from your list of ingredients</p>
--- src/Components/MainBody.jsx
+++ src/Components/MainBody.jsx
@@ -17,18 +17,28 @@ function MainBody() {
    
     function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
+        const button = document.querySelector('button')
+    
         if (newIngredient) {
-            setIngredients(
-                prevIngredients => [...prevIngredients, newIngredient]
-            )
+            setIngredients(prevIngredients => [...prevIngredients, newIngredient])
         } else {
-            alert("Please enter a valid ingredient")
+            button.classList.add('shake', 'error')
+            setTimeout(() => {
+                button.classList.remove('shake', 'error')
+            }, 400);
         }
     }
 
+    function startOver() {
+        setIngredients([])
+        setRecipe("")
+        setIsLoading(false)
+    }
+    
+
     return (
-        <main className='px-16 py-16'>
-            <form action={addIngredient} className='flex justify-center items-center g-3'>
+        <main className='px-8 py-16 sm:px-16'>
+            <form action={addIngredient} className='flex flex-col justify-center items-center gap-y-3 sm:gap-3 sm:flex-row' >
                 <input
                     type="text"
                     placeholder="e.g. oregano"
@@ -36,12 +46,18 @@ function MainBody() {
                     className="rounded-md border-solid border-2 px-4 py-2 mr-1.5 w-96 grow min-w-fit max-w-lg"
                     name="ingredient"
                 />
-                <button className="bg-black text-gray-200 rounded-md px-12 py-2.5 ml-1.5 font-medium text-sm">+ Add ingredient</button>
+                <button className="bg-black text-gray-200 rounded-md px-6 py-2.5 ml-1.5 font-medium text-sm sm:w-48 md:max-w-72" disabled={recipe ? true : false}>
+                    + Add ingredient
+                </button>
             </form>
-            
             {ingredients.length > 0 && <IngredientsList toggle={generateRecipe} ingArray={ingredients}/>}
             {recipe && <ClaudeRecipe recipe={recipe}/>}
-            {isLoading && !recipe ? <SkeletonLoading /> : ""} 
+            {isLoading && !recipe ? <SkeletonLoading /> : ""}
+            {recipe && <div className="flex justify-center mt-8 sm:mt-16">
+                <button onClick={startOver} className="bg-orange-500 text-white rounded-md px-6 py-2.5 ml-1.5 font-medium">
+                    New Recipe
+                </button>
+            </div>}
         </main>
     )
 }
